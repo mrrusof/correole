503f48531d2030357a46862c2563bd2c169f1f0f
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2016-06-01 00:40:19 -0500

    Rename test for entrypoint.

--- test/bin/correole_spec.rb
+++ test/bin/correole_spec.rb
@@ -1,13 +1,13 @@
 require File.expand_path '../../test_helper.rb', __FILE__
 
-describe 'subscription' do
+describe 'Correole' do
 
   let(:port) { 5000 }
   let(:timeout) { 10 }
   let(:root) { File.expand_path '../../../', __FILE__ }
   let(:cmd) { "PORT=#{port} ruby -I #{root}/lib -I #{root}/config #{root}/bin/correole" }
 
-  it "Correole runs" do
+  it "runs" do
     spawn(cmd, [ :err, :out ] => '/dev/null')
     stop = Time.now.to_i + 10
     while ! system("lsof -i TCP:#{port}", [ :err, :out ] => '/dev/null') && Time.now.to_i < stop
