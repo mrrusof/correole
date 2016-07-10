ab0a6d955078aa98d685932b087cb734ca0a9c4a
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2016-07-09 22:14:43 -0500

    Set subscriber.email not null.

--- db/migrate/0001_create_database.rb
+++ db/migrate/0001_create_database.rb
@@ -1,7 +1,7 @@
 class CreateDatabase < ActiveRecord::Migration
   def change
     create_table :subscribers do |t|
-      t.string :email
+      t.string :email, null: false
       t.index :email, unique: true
       t.timestamps null: false
     end
